d7e8ec3592e59bc4d00268abef895a55a9ee4fcf
Ry6ens <ry6ens@gmail.com>
2022-06-21 14:17:42 +0000

    Deploying to gh-pages from @ Ry6ens/100rokiv@ed0a6e287f20ded7885357462cbfb2c01b227022 🚀

--- menu.366124c2.js
+++ menu.366124c2.js
@@ -1,2 +1,2 @@
 !function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-active"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.4e14cb7d.js.map
+//# sourceMappingURL=menu.366124c2.js.map
